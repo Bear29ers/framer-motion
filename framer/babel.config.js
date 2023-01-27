389b991dbd8c9f3b3f669a27e8b8d2c7aa40aeb4
Yuta Okuma <y.dream.nest@icloud.com>
2023-01-27 11:05:09 +0900

    feat: setup styleint & revise eslint setting

---
+++ framer/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = (api) => {
+  api.cache(true);
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: 'usage',
+          corejs: 3,
+        },
+      ],
+    ],
+  };
+};
