ab7f8ab616e56a8624e8b3d36b508dbce1263a95
Yuta Okuma <y.dream.nest@icloud.com>
2023-02-01 16:17:41 +0900

    feat: styled top page

--- framer/src/App.tsx
+++ framer/src/App.tsx
@@ -1,13 +1,9 @@
 import React, { FC, memo } from 'react';
-import { Link } from 'react-router-dom';
+
+import { Top } from './components/Top';
 
 import './scss/styles.scss';
 
 export const App: FC = memo(() => {
-  return (
-    <>
-      <h1>App Component</h1>
-      <Link to='/expandable-card'>Expandable Card</Link>
-    </>
-  );
+  return <Top />;
 });
